713bcbf52f87f9fd3faa7f2947a43ea6aaa1ecfd
Juan Manuel <ramollino@gmail.com>
2013-01-22 15:42:45 -0300

    change msg

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ include_once("lib/secret.php");
 
 $twitterObj = new EpiTwitter($consumer_key, $consumer_secret);
 $oauth_token = $_GET["oauth_token"];
-$msg = "I have downloaded an eCoupon from www.ramollino.com.ar/LenovoTest/ saving up to $550 on a Lenovo IdeaPad Z570!!";
+$msg = "I have downloaded an eCoupon from www.lenovo-promos.orchestra.io/index.php saving up to $550 on a Lenovo IdeaPad Z570!!";
 
 	if($oauth_token == '')
 	{
